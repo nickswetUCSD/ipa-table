977796ee43a0bdc2dc31640d2f9f3b537578580b
nick swetlin <nswetlin@ucsd.edu>
2024-02-28 17:05:11 -0800

    basic features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>This will be the webpage!!</h1>
-<p>:0</p>
+<script>
+import App from '../components/App.svelte'
+</script>
+
+<body>
+    <h1>This will be the webpage!</h1>
+    <p></p>
+    <App />
+</body>
