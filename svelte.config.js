67f3e198f6ba5f312a4ea3ed99017e91f935f0b4
nick swetlin <nswetlin@ucsd.edu>
2024-03-01 17:20:07 -0800

    fixed base paths 4

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+			relative: false
 		}
+		
 	}
 };
 
